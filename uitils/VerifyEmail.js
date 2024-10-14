5cc2e0dc7385acd6e93392e83169f40116c601bb
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-10-14 06:09:04 +0300

    add VerifyEmail in uitils file

---
+++ uitils/VerifyEmail.js
@@ -0,0 +1,25 @@
+const nodeMailer = require("nodemailer");
+
+module.exports = async (userEmail, subject, htmlTemplate) => {
+  try {
+    const transporter = nodeMailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.APP_EMAIL_ADDRESS, // sender
+        pass: process.env.APP_EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL_ADDRESS, // sender
+      to: userEmail,
+      subject: subject,
+      html: htmlTemplate,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email Sent : ", info.response);
+  } catch (error) {
+    console.log("internal server error ( nodeMailer )");
+  }
+};
